24d0d0086572b9574a39833dfc89b7071cd1d46c
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-11 21:26:49 +0200

    done with navbar. added and styled all nav items

--- auth/app/components/navbar/navbar.tsx
+++ auth/app/components/navbar/navbar.tsx
@@ -1,22 +1,29 @@
 import Link from "next/link"
 
 export default function NavBar(){
-    return <div>
-        <div>
-            <div>
-                <div></div>
-                <Link>Home</Link>
-                <Link>Web Designs</Link>
-                <Link>Mobile Designs</Link>
-                <Link>Illustration</Link>
+    return <div className="fixed top-0 bg-white w-full shadow-sm">
+        <div className="mx-8 mt-5">
+            <div className="flex flex-row justify-between items-center">
+                <div className="flex flex-row items-center space-x-5">
+                    <div className="h-[40px] w-[40] bg-slate-300 rounded-full"></div>
+                    <Link href="/">Home</Link>
+                    <Link href="/">Web Designs</Link>
+                    <Link href="/">Mobile Designs</Link>
+                    <Link href="/">Illustration</Link>
+                </div>
+
+                <div className="flex flex-row justify-between items-center space-x-5">
+                    <div className="">
+                        <input className="border rounded-full text-center py-1 px-2" type="text" placeholder="search" />
+                    </div>
+                    <div>
+                        <Link href="/login">Sign up</Link>
+                    </div>
+                    <div>
+                        <Link href="/signup">Login</Link>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div>
-            <div>
-                
-            </div>
-            <div></div>
-            <div></div>
         </div>
     </div>
 }
---
+++ auth/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function signup(){
+    return <div>
+        <div>
+            <h1>signup</h1>
+        </div>
+    </div>
+}
