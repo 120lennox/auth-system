27b2d1b89845746154a21147e8d33335ad21cfe8
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-11 20:06:31 +0200

    added nav items

--- auth/app/components/navbar/navbar.tsx
+++ auth/app/components/navbar/navbar.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link"
+
 export default function NavBar(){
     return <div>
         <div>
-            
+            <div>
+                <div></div>
+                <Link>Home</Link>
+                <Link>Web Designs</Link>
+                <Link>Mobile Designs</Link>
+                <Link>Illustration</Link>
+            </div>
+        </div>
+        <div>
+            <div>
+                
+            </div>
+            <div></div>
+            <div></div>
         </div>
     </div>
 }
