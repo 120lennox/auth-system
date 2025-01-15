c122e3a7d609ee13ac8a7ac581f6eee56cfca70e
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-15 20:38:37 +0200

    styling signup page

--- auth/app/login/page.tsx
+++ auth/app/login/page.tsx
@@ -5,19 +5,20 @@ export default function login(){
     return <div>
         <div className="flex justify-center items-center h-screen">
             <div className="bg-white w-[455] h-[491] rounded-2xl flex flex-col items-center">
-                <div className="flex flex-row bg-orange-700 w-[350] justify-between px-10 py-2 rounded-xl mt-8">
-                    <div className="bg-white">
-                        <Link href="/signup">Sign Up</Link>
+                <div className="flex flex-row w-full max-w-[450] px-10 py-2 rounded-xl mt-8">
+                    <div className="bg-cyan py-2 w-1/2 rounded-l-xl ">
+                        <Link className="text-center flex justify-center items-center text-white" href="/signup">Sign Up</Link>
                     </div>
-                    <div>
-                        <Link href="/">Login</Link>
+                    <div className="bg-slate-400 w-1/2 py-2 rounded-r-xl">
+                        <Link className="flex justify-center items-center text-white" href="/">Login</Link>
                     </div>
                 </div>
-                <div>
+                <div className="text-[23px] mt-5 font-medium">Signup</div>
+                <div className="">
                     <Link href="">Sign up with  Facebook</Link>
                 </div>
                 <div>
-                    <Link href="Signup with Google"></Link>
+                    <Link href="">Signup with Google</Link>
                 </div>
                 <div>
                         <div><hr /></div>
