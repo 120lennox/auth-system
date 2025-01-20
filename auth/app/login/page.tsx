30b5642b41ae87a037185b95bd57d10fec42ba99
Lennox Mountain <lennoxmountain67@gmail.com>
2025-01-20 19:14:05 +0200

    added new container for login items

--- auth/app/login/page.tsx
+++ auth/app/login/page.tsx
@@ -2,8 +2,36 @@ export default function login(){
     return <div>
         <div className="flex justify-center items-center h-screen text-sm mt-10 duration-300 ease-in-out">
             <div className="bg-white text-slate-800 shadow-lg w-[552] mx-h-[838] rounded-2xl">
-                <div className="flex flex-col space-y-5 py-5">
+                <div className="flex flex-col mx-5 space-y-5 py-5">
+                    <div className="text-center text-[24px] ">Login</div>
+                    <form action="">
+                        <div>
+                            <label htmlFor="email/username">Email or Username</label>
+                            <input type="text" id="email/username" placeholder="Enter your email ot username" />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input type="text" id="password" placeholder="Enter your password" />
+                        </div>
+                        <div>
+                            <input type="checkbox" />
+                            <label htmlFor="policy">By creating an account, I agree to our Terms of use and Privacy Policy </label>
+                        </div>
+                        <div>
+                            <button>Login</button>
+                        </div>
+                    </form>
                     
+                    <div>
+                        <div><hr /></div>
+                        <div>or</div>
+                        <div><hr /></div>
+                    </div>
+                    <div>continue with google</div>
+                    <div>
+                        <p>Already have an account?</p>
+                        <button>Signup</button>
+                    </div>
                 </div>
             </div>
         </div>
